1012eb81679ff6a8dd8970fb1f46c2191f18b885
Duc Nguyen <ducnh1702@gmail.com>
2026-02-23 14:24:07 +0700

    Add Generic repositories for LessonAPI

--- TechTrioCourses_BE/LessonService/LessonAPI.Application/Interfaces/ILessonRepository.cs
+++ TechTrioCourses_BE/LessonService/LessonAPI.Application/Interfaces/ILessonRepository.cs
@@ -1,15 +1,12 @@
 using LessonAPI.Domain.Entities;
+using TechTrioCourses.Shared.Repositories.Interfaces;
 
 namespace LessonAPI.Application.Interfaces
 {
-    public interface ILessonRepository
+    public interface ILessonRepository : IGenericRepository<Lesson>
     {
-        Task<IEnumerable<Lesson>> GetAllAsync();
+        
         Task<IEnumerable<Lesson>> GetAllLessonByCourseAsync(Guid courseId);
-        Task<Lesson?> GetByIdAsync(Guid id);
-        Task<Lesson> CreateAsync(Lesson course);
-        Task<Lesson?> UpdateAsync(Lesson course);
-        Task<bool> DeleteAsync(Guid id);
-        Task<bool> ExistsAsync(Guid id);
+       
     }
 }
--- TechTrioCourses_BE/LessonService/LessonAPI.Infrastructure/Repositories/LessonRepository.cs
+++ TechTrioCourses_BE/LessonService/LessonAPI.Infrastructure/Repositories/LessonRepository.cs
@@ -3,24 +3,20 @@ using LessonAPI.Domain.Entities;
 
 using Microsoft.EntityFrameworkCore;
 using LessonAPI.Application.Interfaces;
+using TechTrioCourses.Shared.Repositories;
 
 namespace LessonAPI.Infrastructure.Repositories
 {
-    public class LessonRepository : ILessonRepository
+    public class LessonRepository : GenericRepository<Lesson,LessonDbContext>,ILessonRepository
     {
-        private readonly LessonDbContext _context;
+       
 
-        public LessonRepository(LessonDbContext context)
-        {
-            _context = context;
+        public LessonRepository(LessonDbContext context) :base(context) {
+        
+           
         }
-
-        public async Task<IEnumerable<Lesson>> GetAllAsync()
-        {
-            return await _context.Lessons
-                
-                .ToListAsync();
-        }
+        protected override DbSet<Lesson> DbSet => _context.Lessons;
+        
         public async Task<IEnumerable<Lesson>> GetAllLessonByCourseAsync(Guid courseId)
         {
             return await _context.Lessons
@@ -29,68 +25,6 @@ namespace LessonAPI.Infrastructure.Repositories
                 .ToListAsync();
         }
 
-        public async Task<Lesson?> GetByIdAsync(Guid id)
-        {
-            return await _context.Lessons
-                
-                .FirstOrDefaultAsync(c => c.Id == id);
-        }
-
-        public async Task<Lesson> CreateAsync(Lesson course)
-        {
-            course.Id = Guid.NewGuid();
-            course.CreatedAt = DateTime.UtcNow;
-            course.UpdatedAt = DateTime.UtcNow;
-
-            _context.Lessons.Add(course);
-            await _context.SaveChangesAsync();
-
-            return course;
-        }
-
-        public async Task<Lesson?> UpdateAsync(Lesson course)
-        {
-            var existingLesson = await _context.Lessons.FindAsync(course.Id);
-            if (existingLesson == null)
-            {
-                return null;
-            }
-
-            course.UpdatedAt = DateTime.UtcNow;
-            _context.Entry(existingLesson).CurrentValues.SetValues(course);
-
-            try
-            {
-                await _context.SaveChangesAsync();
-                return existingLesson;
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!await ExistsAsync(course.Id))
-                {
-                    return null;
-                }
-                throw;
-            }
-        }
-
-        public async Task<bool> DeleteAsync(Guid id)
-        {
-            var course = await _context.Lessons.FindAsync(id);
-            if (course == null)
-            {
-                return false;
-            }
-
-            _context.Lessons.Remove(course);
-            await _context.SaveChangesAsync();
-
-            return true;
-        }
-
-        public async Task<bool> ExistsAsync(Guid id)
-        {
-            return await _context.Lessons.AnyAsync(e => e.Id == id);
-        }
+       
     }
 }
